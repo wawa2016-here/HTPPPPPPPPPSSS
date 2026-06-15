e78a1d03eb248cdff82c5ccc5aed498f2a37686e
Wawa2016-here <lemver2016@outlook.com>
2026-06-15 15:29:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let text = "<Game> under This text"
+function initGame() { 
+
+}
+ const btn = document.getElementById('gameBtn');  
+        btn.onclick = function() { 
+            initGame(); 
+            console.log("Started");  
+document.getElementById("new").textContent = text; 
+        }; 
