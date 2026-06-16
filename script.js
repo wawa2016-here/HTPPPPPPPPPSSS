ecd8d206bb5a279801c38191a8005930c78d129c
Wawa2016-here <lemver2016@outlook.com>
2026-06-16 17:03:17 -0500

    script.js

--- script.js
+++ script.js
@@ -7,50 +7,47 @@ var gameAttacksEnemy = ["Dungon_Power50", "underpower_Power40"];
 let text = "Game under This text";
 
 function initGame() { 
- console.log("Started Game");  
+  console.log("Started Game");  
   document.getElementById("new").textContent = text; 
-  setInterval(() => { 
- window.addEventListener("keydown", function(event) {
-  if (event.key === "1") {
-    gameHealthEnemy -= 350;
-    enemyAttackNumber = Math.floor(Math.random() * 2) + 2;
-    if (enemyAttackNumber === 2) {
-      gameHealthPlayer -= 50;
-    } else if (enemyAttackNumber === 3) {
-      gameHealthPlayer -= 40;
-    }
-  } else if (event.key === "2") {
-    gameHealthEnemy -= 100;
-    enemyAttackNumber = Math.floor(Math.random() * 2) + 2;
-    if (enemyAttackNumber === 2) {
-      gameHealthPlayer -= 50;
-    } else if (enemyAttackNumber === 3) {
-      gameHealthPlayer -= 40;
-    }
-  } else if (event.key === "3") {
-    gameHealthEnemy -= 40;
-    enemyAttackNumber = Math.floor(Math.random() * 2) + 2;
-    if (enemyAttackNumber === 2) {
-      gameHealthPlayer -= 50;
-    } else if (enemyAttackNumber === 3) {
-      gameHealthPlayer -= 40;
-    }
-  }
   
-  document.getElementById("game-stats").textContent = gameStats;
-  document.getElementById("game-health-player").textContent = gameHealthPlayer;
-  document.getElementById("game-health-enemy").textContent = gameHealthEnemy;
-  document.getElementById("game-attacks-player").textContent = gameAttacksPlayer;
-  document.getElementById("game-attacks-enemy").textContent = gameAttacksEnemy;
+  window.addEventListener("keydown", function(event) {
+    if (event.key === "1") {
+      gameHealthEnemy -= 350;
+      enemyAttackNumber = Math.floor(Math.random() * 2) + 2;
+      if (enemyAttackNumber === 2) {
+        gameHealthPlayer -= 50;
+      } else if (enemyAttackNumber === 3) {
+        gameHealthPlayer -= 40;
+      }
+    } else if (event.key === "2") {
+      gameHealthEnemy -= 100;
+      enemyAttackNumber = Math.floor(Math.random() * 2) + 2;
+      if (enemyAttackNumber === 2) {
+        gameHealthPlayer -= 50;
+      } else if (enemyAttackNumber === 3) {
+        gameHealthPlayer -= 40;
+      }
+    } else if (event.key === "3") {
+      gameHealthEnemy -= 40;
+      enemyAttackNumber = Math.floor(Math.random() * 2) + 2;
+      if (enemyAttackNumber === 2) {
+        gameHealthPlayer -= 50;
+      } else if (enemyAttackNumber === 3) {
+        gameHealthPlayer -= 40;
+      }
+    }
+  });
 
-},4); 
-
-});
+  setInterval(() => { 
+    document.getElementById("game-stats").textContent = gameStats;
+    document.getElementById("game-health-player").textContent = gameHealthPlayer;
+    document.getElementById("game-health-enemy").textContent = gameHealthEnemy;
+    document.getElementById("game-attacks-player").textContent = gameAttacksPlayer;
+    document.getElementById("game-attacks-enemy").textContent = gameAttacksEnemy;
+  }, 4); 
 }
 
 const btn = document.getElementById('gameBtn');  
 btn.onclick = function() { 
   initGame(); 
-  
 };
-
